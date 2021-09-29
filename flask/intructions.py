0fa92f2c63d84e5999881a13784ce5f21e07d11a
[Alexandro Gomez] <alex.gmzc@gmail.com>
2021-09-28 20:12:08 -0500

    flask venv

--- flask/intructions.py
+++ flask/intructions.py
@@ -1,2 +1,15 @@
 print("Flask work instructions")
 
+for activate the venv I need:
+    > cd venv\Scripts\activate.bat
+
+For run app.py I need:
+    >python app.py
+
+For test if flask server is running:
+    > http://127.0.0.1:5000/
+
+For deactivate venv, I need:
+    > cd\venv\Scripts\deactivate.bat
+
+
